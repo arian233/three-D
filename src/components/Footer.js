df7a4034b73d4376d3fca3fc1ca0978ddf4a33a2
Arian Ansari <esfahanarian@gmail.com>
2023-10-16 23:24:12 -0700

    Footer fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 const Footer = () => {
     return (
-        <footer className="absolute bottom-1 w-full flex justify-center">
+        <footer className="bottom-1 w-full flex justify-center bg-slate-800">
             <p className="text-[#f9a142] text-lg">Arian Ansari. @2023</p>
         </footer>
     );
